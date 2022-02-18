0082faca3a5b2935ea36d28f9dd9110cc651a75d
mlj1029 <99918274+mlj1029@users.noreply.github.com>
2022-02-18 10:04:59 -0600

    Update most_popular_data.py

--- most_popular_data.py
+++ most_popular_data.py
@@ -1,5 +1,5 @@
 import psycopg2
-conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=Waj62mWaj62m")
+conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=******")
 cur = conn.cursor()
 """ connect to postgres and load the csv file into the database """ 
 with open('du_poc/most_popular.csv', 'r') as f:
