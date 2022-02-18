854c692d65d6956e564f03a144d756121f09759b
mlj1029 <99918274+mlj1029@users.noreply.github.com>
2022-02-18 09:32:00 -0600

    Add files via upload

---
+++ country_weekly.py
@@ -0,0 +1,17 @@
+import psycopg2
+conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=Waj62mWaj62m")
+cur = conn.cursor()
+cur.execute("""
+    CREATE TABLE country_weekly(
+        country_name VARCHAR(50) NOT NULL,
+        country_iso2 VARCHAR(20) NOT NULL,
+        week DATE,
+        category VARCHAR(20),
+        weekly_rank INTEGER,
+        show_title VARCHAR(200),
+        season_title VARCHAR(200),
+        cumulative_weeks_in_top_10 INTEGER
+    )
+""")
+conn.commit()
+
